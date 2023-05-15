e31c637e04ec842b72b22fcb2f49046536b2c811
mazenfteha <mazn34903@gmail.com>
2023-05-14 18:08:02 -0700

    create signup routes

--- djangoChat/core/views.py
+++ djangoChat/core/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm 
 
 def frontpage(request):
     return render(request, 'core/frontpage.html')
+
+def signup(request):
+    if request.method == 'POST': #check if the form submitted
+        form = SignUpForm(request.POST)
+
+        #check if it valid
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {'form':form})
